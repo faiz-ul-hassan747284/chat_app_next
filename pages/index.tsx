41f9829c97de9f8dcda4660f8852c85d175e4944
Faiz Ul Hassan <faizulhassan747284@gmail.com>
2023-04-14 00:31:37 +0500

    add basic landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
+
 import Navbar from "../components/shared/Navbar";
 
+import style from './Landing.module.css'
 export default function Home() {
   return (
     <main>
       <Navbar/>
+      <div className={style.landing_div}>
+      <div className={style.landing_div_inner}>
+        <h1 className={style.landing_div_heading}>This is a Chat App</h1>
+        </div>
+        <hr/>
+        Looking for a better way to stay connected with friends and family? Try our new chat app! With our app, you can easily send messages, make voice and video calls, share photos and videos, and much more.
+      </div>
     </main>
   )
 }
