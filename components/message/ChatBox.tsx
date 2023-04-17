e6ff250e82abce71b27c28da27d33e5a90e75ae2
Faiz Ul Hassan <faizulhassan747284@gmail.com>
2023-04-18 04:46:59 +0500

    add styling to message form and message

--- components/message/ChatBox.tsx
+++ components/message/ChatBox.tsx
@@ -1,3 +1,5 @@
+import { BsFillSendFill } from "react-icons/bs";
+
 import { messagesData } from "@/lib/types";
 import style from "./ChatBox.module.css";
 
@@ -7,9 +9,21 @@ function ChatBox({ messages }: { messages: messagesData }) {
   return (
     <div className={style.chat_box_wrapper}>
       <div className={style.chat_box}>
-        {messages.message.map((message, i) => (
-          <Message key={i} data={message}/>
-        ))}
+        <div>
+          {messages.message.map((message, i) => (
+            <Message key={i} data={message} />
+          ))}
+        </div>
+        <div className={style.message_form}>
+          <input
+            className={style.message_input}
+            type="text"
+            placeholder="Message..."
+          />
+          <button className={style.message_input_button}>
+            <BsFillSendFill />
+          </button>
+        </div>
       </div>
     </div>
   );
