f50abd14ee55ed8b916e29895d774bd9b19bc3b6
Faiz Ul Hassan <faizulhassan747284@gmail.com>
2023-04-17 05:38:50 +0500

    add api base

--- components/message/ChatBox.tsx
+++ components/message/ChatBox.tsx
@@ -1,6 +1,10 @@
 import style from "./ChatBox.module.css";
-function ChatBox() {
-  return <div className={style.chat_box_wrapper}><div className={style.chat_box}>ok</div></div>;
+function ChatBox({messages}) {
+  return (
+    <div className={style.chat_box_wrapper}>
+      <div className={style.chat_box}>{JSON.stringify(messages)}</div>
+    </div>
+  );
 }
 
 export default ChatBox;
--- pages/message.tsx
+++ pages/message.tsx
@@ -1,12 +1,20 @@
 import Navbar from "@/components/shared/Navbar";
 import ChatBox from "@/components/message/ChatBox";
-function Message() {
+function Message({data}) {
   return (
     <main>
       <Navbar />
-      <ChatBox/>
+      <ChatBox messages={data}/>
     </main>
   );
 }
 
 export default Message;
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.API_BASE}api/messages`);
+  const data = await res.json();
+  return {
+    props: {data}, // will be passed to the page component as props
+  }
+}
