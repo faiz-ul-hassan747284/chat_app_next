669d682abd18ff5eb0dabb7d411116bdf53e3c4a
Faiz Ul Hassan <faizulhassan747284@gmail.com>
2023-04-20 01:07:29 +0500

    add login and logout logic

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,22 +1,30 @@
 import Link from "next/link";
 import { Montserrat } from "next/font/google";
 import styles from "./Navbar.module.css";
-
+import { useContext } from "react";
+import { UserContext } from "@/context/UserContext";
 const montserrat = Montserrat({
   weight: "400",
   subsets: ["latin"],
 });
 
 function Navbar() {
+  const { setUserName, userName } = useContext(UserContext);
   return (
     <div className={`${montserrat.className} ${styles.navbar}`}>
       <div className={styles.navbar_flex}>
-      <div>
-      <Link className={styles.navbar_text} href='/message'>  Chat App </Link>
-      <Link className={styles.navbar_text} href='https://github.com/faiz-ul-hassan747284'> About me </Link>
-      </div>
+        <div>
+          <Link className={styles.navbar_text} href='/'>  Chat App </Link>
+          <Link className={styles.navbar_text} href='/message'>  Start Message </Link>
+          <Link className={styles.navbar_text} href='https://github.com/faiz-ul-hassan747284'> About me </Link>
+        </div>
 
-      <Link className={styles.navbar_text} href='/message'>  Log-in </Link>
+        {userName === '' ? (
+          <Link className={styles.navbar_text} href='/message'>  Log-in </Link>
+        ) : (
+          <Link href='/' onClick={() => setUserName('')} className={styles.navbar_text} >  Log-Out </Link>
+        )}
+
       </div>
     </div>
   );
