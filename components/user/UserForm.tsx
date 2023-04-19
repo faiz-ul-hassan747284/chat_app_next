1752fd02a2d7f6bdd0c0d52f78b4e44028a2b768
Faiz Ul Hassan <faizulhassan747284@gmail.com>
2023-04-19 05:13:01 +0500

    add user form and link it with context

--- components/user/UserForm.tsx
+++ components/user/UserForm.tsx
@@ -1,13 +1,36 @@
+import { useState, useContext, KeyboardEvent } from "react";
+import { useRouter } from 'next/router'
+
 import style from "./UserForm.module.css";
+import { UserContext } from "@/context/UserContext";
 
 function UserForm() {
-
-
+  const [name, setName] = useState("");
+  const { setUserName } = useContext(UserContext);
+  const router = useRouter()
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+  function handleNameKeyDown(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter" && name.trim()) {
+      setUserName(name)
+      router.back()
+    }}
   return (
     <div className={style.user_form_wrapper}>
+      <h1 className={style.name_label}>
+        Enter User Name
+      </h1>
       <div className={style.user_form}>
-      ok
+        <input
+          placeholder="Awesome Name..."
+          className={style.name_input}
+          value={name}
+          onChange={handleNameChange}
+          onKeyDown={handleNameKeyDown}
+        />
       </div>
+
     </div>
   );
 }
