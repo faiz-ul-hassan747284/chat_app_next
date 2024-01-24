0f41614411f4b05b39ee0a1fa6bf044156742479
Faiz Ul Hassan <faizulhassan747284@gmail.com>
2024-01-23 19:56:29 -0800

    Update userAuthentication.tsx

--- authentication/userAuthentication.tsx
+++ authentication/userAuthentication.tsx
@@ -9,11 +9,22 @@ function UserAuthentication({ children }: { children: React.ReactElement }) {
   useEffect(() => {
     // check if the user is not logged in and is not on the home page
     if (!userName && router.pathname !== "/") {
-      router.push("/user");
+      let path = router.asPath;
+      if (router.asPath.includes('callback_url=/user?')) {
+        path = path.replace(/user\?callback_url=\//g, '');
+      }
+      console.log(path)
+
+      router.push(`/user?callback_url=${path}`);
     }
-    if (userName && router.pathname==='/user')
-      router.back()
-  }, [userName, router.pathname, router]);
+    if (userName && router.asPath.includes('/user')) {
+      const { callback_url } = router.query;
+      console.log('callback_url',callback_url)
+      if (callback_url) {
+        router.push(String(callback_url))
+      }
+      else { router.back(); }
+    }}, [userName, router.pathname, router]);
 
   return children;
 }
